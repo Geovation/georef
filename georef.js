0609ed339de27bbcb9eb2c7d6fb2f8e68968299c
Max Harlow <max.harlow@os.uk>
2016-06-29 16:30:54 +0100

    Naming again

--- georef.js
+++ georef.js
@@ -2,7 +2,7 @@
 //
 // curl -v \
 //      -F 'image=@test.tiff' \
-//      -F 'points=[{ "imgX": 83, "imgY": 754, "geoX": -0.11243820190429688, "geoY": 51.52178189877856 },{ "imgX": 262, "imgY": 261, "geoX": -0.11057138442993164, "geoY": 51.525052857123434 },{ "imgX": 594, "imgY": 685, "geoX": -0.10698795318603516, "geoY": 51.522182436913845 }]' \
+//      -F 'points=[{ "imgLng": 83, "imgLat": 754, "geoLng": -0.11243820190429688, "geoLat": 51.52178189877856 },{ "imgLng": 262, "imgLat": 261, "geoLng": -0.11057138442993164, "geoLat": 51.525052857123434 },{ "imgLng": 594, "imgLat": 685, "geoLng": -0.10698795318603516, "geoLat": 51.522182436913845 }]' \
 // localhost:3000/georeference
 
 const FS = require('fs')
@@ -31,7 +31,7 @@ app.post('/georeference', upload.single('image'), function (request, response) {
 app.listen(3030)
 
 function georeference(filepath, points, callback) {
-    const control = points.map(point => `-gcp ${point.imgX} ${point.imgY} ${point.geoX} ${point.geoY}`).join(' ')
+    const control = points.map(point => `-gcp ${point.imgLng} ${point.imgLat} ${point.geoLng} ${point.geoLat}`).join(' ')
     const translateCommand = `gdal_translate -of GTiff -a_srs EPSG:4326 ${control} ${filepath} ${filepath}-geo`
     ChildProcess.exec(translateCommand, e => {
         if (e) callback(e)
