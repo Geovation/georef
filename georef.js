13fdabfa925c47fa7e379e22b1c1cb093f292583
Max Harlow <max.harlow@os.uk>
2016-08-04 14:27:39 +0100

    Check code too

--- georef.js
+++ georef.js
@@ -106,7 +106,7 @@ function upload(id, data, callback) {
     }
     Request.post(request, (e, response) => {
         if (e) callback(e)
-        else if (response.body === '') callback({ message: 'System error' })
+        else if (response.statusCode >= 400 && response.body === '') callback({ message: response.statusCode })
         else if (response.statusCode >= 400) callback(JSON.parse(response.body))
         else callback(null)
     })
